d30909e8d943b874b66137797fedfd13ac9d9c10
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-03-30 00:20:06 -0300

    Atualização aula 01

--- Aula-01/index.js
+++ Aula-01/index.js
@@ -1,16 +1,34 @@
-// IMUTABILIDADE
+/**
+ * IMUTABILIDADE
+ * 
+ * Significa que uma variável que nasceu com um valor, vai manter esse valor enquanto o programa estiver sendo executado, sem mudar seu estado. Ou seja, ela é uma constante.
+ * 
+ * Stateless (Sem estado)
+ * 
+ * As funções devem ser sem estado (stateless), ou seja, elas devem sempre agir e retornar algo como se fosse a primeira vez que elas fossem utilizadas no programa.
+ */
+
+// Não Fazer
+let numero1 = 0
 
 // Não fazer
-let numero = 0;
-const somarDezNoNumero = () => numero = numero + 10;
-console.log(somarDezNoNumero());
+const somarDezNoNumero = () => numero1 = numero1 + 10
+somarDezNoNumero()
 
 // Fazer
-const numero1 = 0;
-const somarDezNoNumero1 = () => numero1 + 10;
-console.log(somarDezNoNumero1());
+const numero2 = 0
 
-// STATELESS
+// Fazer
+const somarDez1 = (x) => x + 10
+somarDez1(numero2)
+
+// Fazer
+const somarDez2 = (x) => {
+  let y = x + 10
+
+  return y
+}
+somarDez2(numero2)
 
 // const somar = (x, y) => {
 //     let z
